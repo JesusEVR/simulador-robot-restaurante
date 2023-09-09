4ade5fecf7e01eb2e62911a3a91111f6b46be6aa
paolasanv <sanv.leslie@ciencias.unam.mx>
2023-09-09 15:45:01 -0600

    metodo del queso x2

--- src/HamburguesaCarnivora.java
+++ src/HamburguesaCarnivora.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class HamburguesaCarnivora extends Hamburguesa{
 
+	private Scanner scan =  new Scanner(System.in);
 	private String tipoCarne; //Especifica el tipo de carne que va a llevar (ej. res)
 	
 	public HamburguesaCarnivora(String ID, String nombre, String descripcion,double precio, boolean esVegetariano, String tipoCarne){
@@ -8,9 +11,28 @@ public class HamburguesaCarnivora extends Hamburguesa{
 	}
 	
 	public void ponerQueso(){
+		String opcion;
+		String o;
+		boolean seguir=true;
+			while(seguir){
+				System.out.println("¿Desea queso en su hamburguesa? Sí = s, No = n");
+				opcion = scan.nextLine();
+				o = opcion.toLowerCase();
+			
+				if(o.equals("s")){
+				System.out.println("De acuerdo, con queso");
+					seguir=false;
+				}else if(o.equals("n")){
+					System.out.println("De acuerdo, sin queso");
+					seguir=false;
+				}	
+			}	
 	}
 	public void prepararCarne(){
+		System.out.println("Preparando la carne de " +tipoCarne);
 	}
 	public void ponerCarne(){
+		System.out.println("Poniendo la carne de " +tipoCarne);
 	}
+	
 }
