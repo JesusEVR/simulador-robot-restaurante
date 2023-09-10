f790a476f1d4a268984695378453d003410c12a3
paolasanv <sanv.leslie@ciencias.unam.mx>
2023-09-10 13:45:47 -0600

    interfaz

---
+++ src/Menu.java
@@ -0,0 +1,17 @@
+/**
+* Interfaz que define la característica común de los menú
+*
+*@author paolasanv
+*@author Supr-Lilito
+*@author JesusEVR
+*
+*/
+public interface Menu{
+	
+	/**
+	* Crea un iterador sobre el menú
+	*
+	*/
+	public Iterador creaIterador();
+
+}
