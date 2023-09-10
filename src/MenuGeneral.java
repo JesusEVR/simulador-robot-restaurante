bc12fd8d63e9e7bba8e9e23d018986cd8727c662
paolasanv <sanv.leslie@ciencias.unam.mx>
2023-09-10 13:46:26 -0600

    menuGeneral

---
+++ src/MenuGeneral.java
@@ -0,0 +1,50 @@
+/**
+* Clase que define la característica del menú general
+*
+*@author paolasanv
+*@author Supr-Lilito
+*@author JesusEVR
+*
+*/
+public class MenuGeneral implements Menu{
+
+	/**
+	* Estructura de datos para el menu general
+	*/
+	private Hamburguesa[] menuGeneral = new Hamburguesa[3];
+	/**
+	* Posicion actual del arreglo
+	*/
+	private int posicion=0;
+	
+	
+	/**
+	* Constructor por omisión que inicializa el menú general 
+	*/
+	public MenuGeneral(){
+	 	Hamburguesa h1 = new HamburguesaCarnivora("ID00", "PRUEBA", "Hamburguesa de prueba",17.8, false, "res");
+		agregarHamburguesa(h1);
+		
+	}
+	
+	/**
+	* Metodo que añade hamburgesas al menú
+	*/
+	public void agregarHamburguesa(Hamburguesa h){
+		if(posicion>menuGeneral.length){
+			System.out.println("El menú general está lleno");
+		}else {
+			menuGeneral[posicion] = h;
+			posicion++;
+		}
+	}
+	
+	/**
+	* Metodo que crea un iterador para recorrer los elementos del menú
+	*
+	* @return iterador
+	*/
+	public Iterador creaIterador(){
+		return new MenuGeneralIterador(menuGeneral);
+	}
+}
