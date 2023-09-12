feb52849bf5b46f20c43c14c316c22a7b76e136f
Leslie SanV <111658258+paolasanv@users.noreply.github.com>
2023-09-12 11:46:51 -0600

    BorrarClase

--- src/ModoActivo.java
+++ src/ModoActivo.java
@@ -1,18 +1,1 @@
-public class ModoActivo implements EstadoRobot{
-	
-	public ModoActivo(){
-	}
-	
-	public void suspender(){
-	}
-	public void activar(){
-	}
-	public void caminar(){
-	}
-	public void atender(){
-	}
-	public void cocinar(){
-	}
-	public void entregar(){
-	}
-}
+//no va xd
