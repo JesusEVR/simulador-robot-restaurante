b5f4f988175916fdffe707a296fbedd77f7a1129
JesusEVR <124617308+JesusEVR@users.noreply.github.com>
2023-09-15 09:15:22 -0600

    Cambio en los metodos

--- src/ModoSuspendido.java
+++ src/ModoSuspendido.java
@@ -38,8 +38,7 @@ public class ModoSuspendido implements EstadoRobot{
 	 *
 	 */
 	public void activar(){
-		robot.activarRobot(true);
-		System.out.println("--- He sido activado ---");	
+		System.out.println("** Robot Activado **")	
 		robot.asignarNuevoEstado(robot.modoCaminar());
 		System.out.println("--- Cambiando a modo C A M I N A R ---\n");
 	}
