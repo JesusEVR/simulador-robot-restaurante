610f2c55264767488efec914883385ffbc6f3248
JesusEVR <124617308+JesusEVR@users.noreply.github.com>
2023-09-16 20:15:19 -0600

    Cambios de queso

--- src/HamburguesaCarnivora.java
+++ src/HamburguesaCarnivora.java
@@ -27,38 +27,13 @@ public class HamburguesaCarnivora extends Hamburguesa{
 	 * @param nombre Cadena con el nombre de la hamburguesa
          * @param descripcion Cadena que contiene una breve descripcion de la hamburguesa
          * @param precio El costo de cada hamburguesa (en pesos mexicanos)
+         * @param tieneQueso Un valor booleano que detalla si la hamburguesa tiene queso (1) o no (0)
          * @param esVegetariano Un valor booleano que detalla si la hamburguesa es vgetariana (1) o no lo es (0)
 	 * @param tipoCarne El tipo de carne que tendra la hamburguesa
 	 */
-	public HamburguesaCarnivora(String id, String nombre, String descripcion,double precio, boolean esVegetariano, String tipoCarne){
-		super(id, nombre, descripcion, precio, esVegetariano);
+	public HamburguesaCarnivora(String id, String nombre, String descripcion, double precio, boolean tieneQueso, boolean esVegetariano, String tipoCarne){
+		super(id, nombre, descripcion, precio, tieneQueso, esVegetariano);
 		this.tipoCarne = tipoCarne;
-	}
-
-	/**
-	 * Metodo implementado que pregunta al comensal si quiere queso en su hamburguesa o no
-         *
-	 */
-	@Override
-	public void ponerQueso(){
-		
-		String opcion;
-		String aMinuscula;
-		boolean seguir = true;
-		
-			while(seguir){
-				System.out.println("--- Dime papu, ¿quieres queso amarrillo en tu hamburguesa? ---\nSí = s, No = n");
-				opcion = scan.nextLine();
-				aMinuscula = opcion.toLowerCase();
-			
-				if(aMinuscula.equals("s")){
-				System.out.println("--- Si le sabes, bro ---");
-					seguir = false;
-				}else if(aMinuscula.equals("n")){
-					System.out.println("--- No te preocupes. No todos pueden tener buenos gustos ---");
-					seguir = false;
-				}	
-			}	
 	}
 
 	/**
